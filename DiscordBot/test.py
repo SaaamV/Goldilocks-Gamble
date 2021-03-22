9193be27c69cd321fed27893fe1d1563cfabd15b
Shivam Taneja <shvmtnja9@gmail.com>
2021-03-22 22:17:39 +0530

    Code

---
+++ DiscordBot/test.py
@@ -0,0 +1,3 @@
+with open('start_message.txt', 'r') as start_message:
+    for line in start_message:
+        print(line)
---
+++ DiscordBot/venv/cogs/example.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+
+class Example(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        await self.client.change_presence(status=discord.Status.online, activity=discord.Game('pain'))
+        print("Bot is ready.")
+
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send(f"Pong!")
+
+
+def setup(client):
+    client.add_cog(Example(client))
