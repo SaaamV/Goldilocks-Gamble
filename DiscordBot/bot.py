40ccdd953f41acd4dd28dddbbbc0d8b4a95494b3
SaaamV <samvarshney.sv@gmail.com>
2021-03-24 15:39:06 +0530

    added initialise function

--- DiscordBot/bot.py
+++ DiscordBot/bot.py
@@ -7,7 +7,45 @@ from crisis import *
 
 intents = discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
 client = commands.Bot(command_prefix = '.', intents = intents)
+#Passive parameter chart
+size = {"Small":1, "Medium":2, "Large":3} #large is good
+distance = {"Close":2, "Ideal":3, "Far":1} #ideal is good
+mass = {1:"Light", 2:"Medium", 3:"Heavy"} 
 
+def initialise():
+    df=pd.read_csv('data.csv')
+    param = {}
+    #Active parameter chart
+    param["air"] = 0.17 #percent oxygen
+    water = {1:50, 2:60, 3:70}
+    temp = {2:17 , 3:14 , 1:11}
+    #Passive parameters
+    for i in range(5):
+        param["size"]=random.choice(list(size.keys()))
+        param["distance"]=random.choice(list(distance.keys()))
+        param["mass"]=mass[size[param["size"]]]
+
+        df.loc[i,'air']=param['air']
+        df.loc[i,'size']=param['size']
+        df.loc[i,'distance']=param['distance']
+        df.loc[i,'mass']=param['mass']
+        #Active parameters
+        param["water"] = water[size[param["size"]]]
+        param["land"] = 100 - param["water"]
+        isPure = False
+        if param["size"] == "Small":
+            isPure = True
+        param["temp"] = temp[distance[param["distance"]]]
+
+        df.loc[i,'water']=param['water']
+        df.loc[i,'land']=param['land']
+        df.loc[i,'temp']=param['temp']
+
+        #Initial resources
+        flora=random.randrange(70,95) #randomising flora diversity
+        df.loc[i,'flora']=flora
+    print(df)
+    df.to_csv('data.csv',index=False)
 #def era side story function
 #Review crisis
  
@@ -58,6 +96,8 @@ async def start(ctx):
             ct=ct+1
             embed.add_field(name=str(ct), value=line, inline=False)
         await ctx.send(embed=embed)
+
+    initialise()
 
 @client.command()
 async def stats(ctx):
