68427ab3b1610f06c21a53f4e13183f2b13ad573
Shashwat G <gargshashwat007@gmail.com>
2021-03-24 13:06:12 +0530

    Stats

--- DiscordBot/test.py
+++ DiscordBot/test.py
@@ -5,6 +5,5 @@ from discord.ext import commands
 import pandas as pd
 df=pd.read_csv('data.csv')
 id = 773227418856587296
-print(df['id']==id)
-print(df.loc[df['id']==id,'name'])
-print(type(df.loc[df['id']==id,'name']))
+print(df.loc[df['id']==id,'name'][1])
+print(type(df.loc[df['id']==id,'name'][1]))
